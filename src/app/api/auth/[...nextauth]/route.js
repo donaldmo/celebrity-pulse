0be562563155e17906dd9a55f39b32f4ce94afee
Donald Motswiri <domotswiri@gmail.com>
2024-09-23 14:42:39 +0200

    next-auth-secrete

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -9,6 +9,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session, token }) {
       if (token) {
